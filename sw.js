51e319897021f86c773a974da971a938dcb7d474
Alexander Kramer <alexander.kramer@timetoact.de>
2026-02-20 23:53:43 +0100

    feat: break shard and sort

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Service Worker – network-first, offline fallback
+const CACHE = 'knack-v1';
+const ASSETS = ['/', '/index.html', '/icon.svg', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
